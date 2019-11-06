f4d71aa100507c193fc2a15d8d35d3d8f44735c4
123shuaideyibi <1428274135@qq.com>
2019-11-06 11:00:20 +0800

    增加antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
 
 class App extends Component {
     render() {
         return (
             <div>
                 后台管理系统
+                <Button>按钮</Button>
             </div>
         );
     }
